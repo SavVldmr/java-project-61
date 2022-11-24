1546ac265ce17071f63fced57b06c0a8a4941ade
SavVldmr <sav.wow@gmail.com>
2022-11-25 01:23:59 +0300

    del app/.gradle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
