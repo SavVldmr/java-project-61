aec7a2cfeefb0c86127d3664746a635cbd73411a
SavVldmr <sav.wow@gmail.com>
2022-11-26 20:37:06 +0300

    add EvenCheckGame.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,17 +6,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
         Scanner console = new Scanner(System.in);
         String response = console.nextLine();
-       switch(response) {
+        switch (response) {
             case "1":
                 Cli.greeting();
                 break;
             case "2":
-
+                EvenCheckGame.startEvenCheckGame();
+                break;
             case "0":
                 System.exit(0);
+                break;
             default:
                 break;
         }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
+    public static String greeting() {
+        System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner console = new Scanner(System.in);
-        String userName = console.nextLine();
-        System.out.println("Hello, " + userName);
+        String username = console.nextLine();
+        System.out.println("Hello, " + username + "!");
+        return username;
     }
 }
