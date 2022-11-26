3557bae3e0930ddf0a476ae929d01f913b5d2071
SavVldmr <sav.wow@gmail.com>
2022-11-26 04:00:17 +0300

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        Scanner console = new Scanner(System.in);
+        String response = console.nextLine();
+       switch(response) {
+            case "1":
+                Cli.greeting();
+                break;
+            case "2":
+
+            case "0":
+                System.exit(0);
+            default:
+                break;
+        }
+
     }
 }
