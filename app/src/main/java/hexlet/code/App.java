e28a3d8ac168a72f17fc5e6f61c4945e18ca861d
SavVldmr <sav.wow@gmail.com>
2022-11-28 01:01:08 +0300

    refactor App.java EvenGame.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,15 @@
 package hexlet.code;
 
 import java.util.Scanner;
+import hexlet.code.games.EvenGame;
+import hexlet.code.games.CalcGame;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner console = new Scanner(System.in);
@@ -16,7 +19,10 @@ public class App {
                 Cli.greeting();
                 break;
             case "2":
-                EvenCheckGame.startEvenCheckGame();
+                EvenGame.startEvenGame();
+                break;
+            case "3":
+                CalcGame.startCalcGame();
                 break;
             case "0":
                 System.exit(0);
--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,11 +1,17 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
-
-public class EvenCheckGame {
-
-    public static void printTheTip() {
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+public class EvenGame {
+    private static final int QUESTIONS_COUNT = 3;
+    public static void startEvenGame() {
+        String tip = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        String[] questions = new String[QUESTIONS_COUNT];
+        String[] correctAnswers = new String[QUESTIONS_COUNT];
+        for (int i = 0; i < QUESTIONS_COUNT; i++) {
+            int questionNumber = getQuestion();
+            questions[i] = String.valueOf(questionNumber);
+            correctAnswers[i] = getCorrectAnswer(questionNumber);
+        }
+        Engine.startGame(tip, questions, correctAnswers);
     }
 
     public static int getQuestion() {
@@ -15,4 +21,5 @@ public class EvenCheckGame {
     public static String getCorrectAnswer(int currentNumber) {
         return (currentNumber % 2 == 0) ? "yes" : "no";
     }
+
 }
